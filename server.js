77225054c199c656e0fac397074626753b4a68cc
Alisher06092003 <qurbonova858@gmail.com>
2025-06-01 21:28:54 +0500

    first 17

--- server.js
+++ server.js
@@ -178,7 +178,8 @@ app.get("/api/groups", async (req, res) => {
                 month: "long",
                 year: "numeric",
                 hour: "2-digit",
-                
+                minute: "2-digit",
+               
             }).format(new Date(group.createdAt))
         }));
 
