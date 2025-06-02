710edcb69db3fecc21b05872768fc88c360887d6
Alisher06092003 <qurbonova858@gmail.com>
2025-06-02 21:47:26 +0500

    manx 5

--- src/script/smsPanel.js
+++ src/script/smsPanel.js
@@ -54,5 +54,13 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-      
+        try {
+            const response = await fetch("http://127.0.0.1:7777/api/create-group", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name: groupName })
+});
+
+
+          
 });
