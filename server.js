ab2f205a9c4eaa8c3dc38e29375a803e85a5e4f8
Alisher06092003 <qurbonova858@gmail.com>
2025-06-04 23:34:14 +0500

    server js n13

--- server.js
+++ server.js
@@ -231,6 +231,7 @@ async function loadGroups() {
         groups.forEach(group => {
             const row = document.createElement("tr");
             row.innerHTML = `
+                <td>${group.name}</td>
                
             `;
             tbody.appendChild(row);
