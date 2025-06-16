5ecfc67beb97fde688a43fef2205bd21e05e3624
Alisher06092003 <qurbonova858@gmail.com>
2025-06-16 22:45:02 +0500

    async function  if length

--- src/script/smsPanel.js
+++ src/script/smsPanel.js
@@ -276,5 +276,10 @@ async function loadGroups() {
         const tbody = document.querySelector("tbody");
         tbody.innerHTML = ""; // Ro'yxatni tozalash
 
-       
+        if (!groups.length) {
+            tbody.innerHTML = "<tr><td colspan='4' class='text-center'>❌ Hech qanday guruh topilmadi!</td></tr>";
+            return;
+        }
+
+      
 }
