f11982c3480aba3e8b17e69bfeacd728d5b8293e
Alisher06092003 <qurbonova858@gmail.com>
2025-06-17 19:52:27 +0500

    findbyan

--- server.js
+++ server.js
@@ -139,6 +139,7 @@ app.put("/api/students/:id", async (req, res) => {
             return res.status(400).json({ message: "❌ Noto‘g‘ri ID formati!" });
         }
 
+        const updatedStudent = await Student.findByIdAndUpdate(
        
 });
 
