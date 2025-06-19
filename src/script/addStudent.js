bc88dfa28b6974c21508b503c8f122133e778f97
Alisher06092003 <qurbonova858@gmail.com>
2025-06-19 22:57:35 +0500

    phone1

--- src/script/addStudent.js
+++ src/script/addStudent.js
@@ -2,7 +2,7 @@ document.getElementById("saveButton").addEventListener("click", async () => {
     const studentData = {
         firstName: document.getElementById("grid-first-name").value.trim(),
       
-       
+        phone1: document.getElementById("phone1").value,
         phone2: document.getElementById("phone2").value,
         group: document.getElementById("grid-state").value
     };
