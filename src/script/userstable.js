29c09801e27099b1e490a2ca52407043de69afab
Alisher06092003 <qurbonova858@gmail.com>
2025-06-20 23:24:55 +0500

    M7

--- src/script/userstable.js
+++ src/script/userstable.js
@@ -306,7 +306,7 @@ function saveUserChanges() {
         firstName: document.getElementById("editFirstName").value,
         lastName: document.getElementById("editLastName").value,
         phone1: document.getElementById("editPhone1").value,
-       
+        phone2: document.getElementById("editPhone2").value,
         
     };
 
