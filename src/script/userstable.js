86656dfc9abb37eb6846af167b0a8179adb94b7e
Alisher06092003 <qurbonova858@gmail.com>
2025-07-01 23:27:28 +0500

    m13

--- src/script/userstable.js
+++ src/script/userstable.js
@@ -133,7 +133,7 @@ document.getElementById("registeredBtn").addEventListener("click", () => {
 document.getElementById("addAdminBtn").addEventListener("click", () => {
     const switchElement = document.querySelector(".elSwitch");
     const iconElement = document.querySelector(".elSwitch svg");
-    
+    const textElement = document.querySelector("#addstuden");
 
     
     
