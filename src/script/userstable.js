e05d3cdf1b0f4b0888efb330503faf080e56cd02
Alisher06092003 <qurbonova858@gmail.com>
2025-07-04 23:33:45 +0500

    c13

--- src/script/userstable.js
+++ src/script/userstable.js
@@ -213,7 +213,7 @@ function deleteSelectedUsers() {
 function showConfirmDialog(text, confirmCallback) {
     Swal.fire({
        
-        
+        icon: "warning",
         iconColor: "red",
         color: "red",
         showCancelButton: true,
