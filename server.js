5457a009aa6cf1a5ca8ba23ab60a17315ebd8831
Alisher06092003 <qurbonova858@gmail.com>
2025-07-08 09:00:59 +0500

    gurux yaratisg b1

--- server.js
+++ server.js
@@ -176,13 +176,7 @@ app.post("/api/create-group", async (req, res) => {
     }
 });
 
-// Guruh nomini tekshirish funksiyasi
-function validateGroupName(name) {
-    if (!name || !name.trim()) {
-        return "❌ Guruh nomi kiritilmadi!";
-    }
-    return null; // Hech qanday xato bo'lmasa null qaytaramiz
-}
+
 
 // Guruhlarni Yuklash 
 app.get("/api/groups", async (req, res) => {
@@ -192,24 +186,18 @@ app.get("/api/groups", async (req, res) => {
         const formattedGroups = groups.map(group => {
             const createdAt = new Date(group.createdAt);
             return {
-                ...group,
-                year: createdAt.getFullYear(),
-                month: createdAt.toLocaleString('uz-UZ', { month: 'long' }),
-                day: createdAt.getDate(),
-                time: createdAt.toLocaleTimeString('uz-UZ', { hour12: false })
+               
+                
+               
+               
             };
         });
 
-        // Sanani "3 June 2025" formatida birlashtirish
-        formattedGroups.forEach(group => {
-            group.formattedDate = `${group.day} ${group.month} ${group.year}`;
-        });
+        
+       
 
-        res.json(formattedGroups);
-    } catch (error) {
-        console.error("❌ Xatolik:", error);
-        res.status(500).json({ message: "❌ Server xatosi!", error: error.message });
-    }
+      
+    
 });
 
 // Serverdan guruhlar ro'yxatini yuklab, uni jadvalda ko'rsatish.
