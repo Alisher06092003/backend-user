170bff1e3350366fbf196c8f6f1f8442d8b7e080
Alisher06092003 <qurbonova858@gmail.com>
2025-07-11 23:39:45 +0500

    f1

--- src/script/addStudent.js
+++ src/script/addStudent.js
@@ -21,31 +21,19 @@ document.getElementById("saveButton").addEventListener("click", async () => {
         });
 
         if (response.ok) {
-            showToast("✅ O‘quvchi muvaffaqiyatli qo‘shildi!", "green");
+         
             // Formani tozalash
             
             
           
             
-            document.getElementById("grid-state").value = 'Gurux Tanlng'; // O'zgarishi mumkin
+            // O'zgarishi mumkin
         } else {
-            showToast("❌ Xatolik yuz berdi!", "red");
+           
         }
-    } catch (error) {
-        console.error("Serverga so‘rov yuborishda xatolik:", error);
-        showToast("⚠️ Ulanish xatosi!", "red");
+   
+       
+        
     }
 });
 
-// Toast xabarini chiqarish funksiyasi
-function showToast(message, color) {
-    const toast = document.getElementById("toast");
-    toast.textContent = message;
-    toast.classList.remove("hidden");
-    toast.classList.add(`bg-${color}-500`);
-
-    setTimeout(() => {
-        toast.classList.add("hidden");
-        toast.classList.remove(`bg-${color}-500`);
-    }, 3000); // 3 sekunddan so‘ng yashirin bo‘ladi
-}
