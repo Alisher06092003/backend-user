6faadaed0bab39413604472ee90ed240798b8577
Alisher06092003 <qurbonova858@gmail.com>
2025-07-27 22:37:35 +0500

    f8

--- src/script/addStudent.js
+++ src/script/addStudent.js
@@ -7,7 +7,15 @@ document.getElementById("saveButton").addEventListener("click", async () => {
         
     };
 
-     
+    // Maydonlarni tekshirish
+    if (!studentData.firstName || !studentData.lastName || studentData.group === "Gurux Tanlng") {
+        
+        return;
+    }
+
+    try {
+        const response = await fetch("http://localhost:7777/api/students", {
+           
            
             
 owToast("✅ O‘quvchi muvaffaqiyatli qo‘shildi!", "green");
