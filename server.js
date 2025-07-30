39b06741a232c07beea7c41eac24f06bb7769eb5
Alisher06092003 <qurbonova858@gmail.com>
2025-07-30 23:17:19 +0500

    API - O‘quvchini qo‘shish (POST) p9

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/api/students', async (req, res) => {
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             phone1: req.body.phone1,
-           
+            phone2: req.body.phone2,
            
         });
 
