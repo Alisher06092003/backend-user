245132cfba58c76cfcadbe8bdf90f316b3939779
Alisher06092003 <qurbonova858@gmail.com>
2025-08-03 23:48:29 +0500

    Guruh nomini tekshirish funksiyasi

--- server.js
+++ server.js
@@ -21,7 +21,13 @@
 
 
 
-
+// Guruh nomini tekshirish funksiyasi
+function validateGroupName(name) {
+    if (!name || !name.trim()) {
+        return "❌ Guruh nomi kiritilmadi!";
+    }
+    return null; // Hech qanday xato bo'lmasa null qaytaramiz
+}
 
 
 
