b7d6319122291beef5a2734f63939b9ccc33794e
Alisher06092003 <qurbonova858@gmail.com>
2025-08-03 23:51:59 +0500

    MongoDB ga ulanish!

--- server.js
+++ server.js
@@ -5,7 +5,17 @@
 
 
 
-
+// MongoDB ga ulanish!
+mongoose.connect('mongodb://127.0.0.1:27017/schoolDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("✅ MongoDB muvaffaqiyatli ulandi!");
+})
+.catch((error) => {
+    console.error("❌ MongoDB ulanishida xatolik:", error);
+});
 
 // API - O‘quvchini qo‘shish (POST)
 app.post('/api/students', async (req, res) => {
