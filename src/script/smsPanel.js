2c4ca4a7fd6c860dc0a0d10268e524e92657372c
Alisher06092003 <qurbonova858@gmail.com>
2025-08-05 23:03:29 +0500

    catchssss

--- src/script/smsPanel.js
+++ src/script/smsPanel.js
@@ -230,7 +230,8 @@ async function loadGroups() {
         });
 
     } catch (error) {
-       
+        console.error("❌ Xatolik:", error);
+        document.querySelector("tbody").innerHTML = "<tr><td colspan='7'>❌ Guruhlar yuklanmadi!</td></tr>";
     }
 }
 
