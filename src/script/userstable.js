8fc9dbf88ce048436c76455ca249726ff94582b7
Alisher06092003 <qurbonova858@gmail.com>
2025-08-10 22:51:09 +0500

    elementlar 6

--- src/script/userstable.js
+++ src/script/userstable.js
@@ -124,7 +124,7 @@ document.getElementById("addStudentsBtn").addEventListener("click", () => {
 
 document.getElementById("addAdminBtn").addEventListener("click", () => {
    
-    
+    const iconElement = document.querySelector(".elSwitch svg");
     const textElement = document.querySelector("#addstuden");
 
     switchElement.style.transform = "translateX(200%)"; // ✅ O‘ngga siljish
