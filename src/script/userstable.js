2119223afeef48ab95c92ed854e0e3a5820f50c6
Alisher06092003 <qurbonova858@gmail.com>
2025-08-13 23:11:41 +0500

    s2

--- src/script/userstable.js
+++ src/script/userstable.js
@@ -114,7 +114,7 @@ document.getElementById("addAdminBtn").addEventListener("click", () => {
 
     
   
-   
+    const textElement = document.querySelector("#addstuden");
 
     switchElement.style.transform = "translateX(100%)"; // ✅ O‘rtaga siljish
     iconElement.style.transform = "rotate(360deg)";
